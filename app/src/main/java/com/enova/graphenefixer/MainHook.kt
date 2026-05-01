57038317709bc6167405a55765e2b6937fe8e225
Enova <17408285+Enovale@users.noreply.github.com>
2026-05-01 03:39:45 -0500

    Hook code doesn't error on either half of the scopes

--- app/src/main/java/com/enova/graphenefixer/MainHook.kt
+++ app/src/main/java/com/enova/graphenefixer/MainHook.kt
@@ -9,51 +9,51 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class MainHook : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        if (lpparam.packageName != "android" && lpparam.packageName != "com.android.settings") return
+        if (lpparam.packageName == "android") {
+            XposedBridge.hookAllMethods(
+                XposedHelpers.findClass(
+                    "android.ext.settings.app.AswRestrictMemoryDynCodeLoading",
+                    lpparam.classLoader
+                ),
+                "getImmutableValue",
+                object : XC_MethodReplacement() {
+                    override fun replaceHookedMethod(param: MethodHookParam?): Any? {
+                        val appInfo = param?.args[2] as? ApplicationInfo?
 
-        XposedBridge.hookAllMethods(
-            XposedHelpers.findClass(
-                "android.ext.settings.app.AswRestrictMemoryDynCodeLoading",
-                lpparam.classLoader
-            ),
-            "getImmutableValue",
-            object : XC_MethodReplacement() {
-                override fun replaceHookedMethod(param: MethodHookParam?): Any? {
-                    val appInfo = param?.args[2] as? ApplicationInfo?
+                        // Settings has to always be allowed for the following patch
+                        if (appInfo != null && appInfo.packageName == "com.android.settings") {
+                            return false
+                        }
 
-                    // Settings has to always be allowed for the following patch
-                    if (appInfo != null && appInfo.packageName == "com.android.settings") {
+                        // All system apps are configurable (null)
+                        if (appInfo != null && (appInfo.flags and ApplicationInfo.FLAG_SYSTEM) != 0) {
+                            param.setResult(null)
+                            return null
+                        }
+
+                        return XposedBridge.invokeOriginalMethod(
+                            param?.method,
+                            param?.thisObject,
+                            param?.args
+                        )
+                    }
+                }
+            )
+        } else if (lpparam.packageName == "com.android.settings") {
+            // All apps are treated equal for the purposes of exploit protections
+            // https://github.com/GrapheneOS/platform_packages_apps_Settings/blob/bf24a0eea505f407837ee53ebe4eec1bd073c99a/src/com/android/settings/users/AppRestrictionsFragment.java#L345
+            XposedBridge.hookAllMethods(
+                XposedHelpers.findClass(
+                    "com.android.settings.users.AppRestrictionsFragment",
+                    lpparam.classLoader
+                ),
+                "isPlatformSigned",
+                object : XC_MethodReplacement() {
+                    override fun replaceHookedMethod(param: MethodHookParam?): Any {
                         return false
                     }
-
-                    // All system apps are configurable (null)
-                    if (appInfo != null && (appInfo.flags and ApplicationInfo.FLAG_SYSTEM) != 0) {
-                        param.setResult(null)
-                        return null
-                    }
-
-                    return XposedBridge.invokeOriginalMethod(
-                        param?.method,
-                        param?.thisObject,
-                        param?.args
-                    )
                 }
-            }
-        )
-
-        // All apps are treated equal for the purposes of exploit protections
-        // https://github.com/GrapheneOS/platform_packages_apps_Settings/blob/bf24a0eea505f407837ee53ebe4eec1bd073c99a/src/com/android/settings/users/AppRestrictionsFragment.java#L345
-        XposedBridge.hookAllMethods(
-            XposedHelpers.findClass(
-                "com.android.settings.users.AppRestrictionsFragment",
-                lpparam.classLoader
-            ),
-            "isPlatformSigned",
-            object : XC_MethodReplacement() {
-                override fun replaceHookedMethod(param: MethodHookParam?): Any {
-                    return false
-                }
-            }
-        )
+            )
+        }
     }
 }
