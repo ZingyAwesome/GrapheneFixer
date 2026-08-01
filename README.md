19f08ad8ce9e3edb549a8c3bf084e0d5cf8d7fc2
ZingyAwesome <development@zingyawesome.com>
2026-08-01 22:12:12 +0100

    Allow configuring DCL via storage for system apps

--- README.md
+++ README.md
@@ -1,28 +1,39 @@
 # GrapheneFixer
 
-GrapheneOS prevents most XPosed modules from operating correctly because XPosed trips DCL code spawning protection.
+GrapheneOS prevents most XPosed modules from operating correctly because XPosed trips DCL code
+spawning protection.
 
-This module allows you to configure all exploit protection for system apps, so you can allow DCL for these problematic system apps.
+This module allows you to configure DCL via memory/storage for system apps, so you can allow DCL for
+these problematic system apps.
 
 *Note*: The Settings app is forced to always allow memory DCL.
 
 ## Installation and Usage
 
 ### 1. Download the APK
-Download the latest APK from [here](https://github.com/Enovale/GrapheneFixer/releases/latest) and install it on your device.
+
+Download the latest APK from [here](https://github.com/Enovale/GrapheneFixer/releases/latest) and
+install it on your device.
 
 ### 2. Enable the Module
-Enable the module in either Xposed or LSPosed.
+
+Enable the module in either XPosed or LSPosed.
 
 ### 3. Enable the right scopes
+
 Select the "Recommended" scopes, i.e. the `android`, and `com.android.settings` scopes.
 
 ### 4. Reboot
+
 Reboot your device for the changes to take effect.
 
 ### 5. Enable DCL for needed modules
-Whenever a module scopes to a system app, make sure to go into that app's info and enable DCL before rebooting.
+
+Whenever a module scopes to a system app, make sure to go into that app's info and enable DCL before
+rebooting.
 
 ---
+
 ## License
+
 This project is licensed under the [MIT License](LICENSE).
--- app/src/main/java/com/enova/graphenefixer/MainHook.kt
+++ app/src/main/java/com/enova/graphenefixer/MainHook.kt
@@ -11,29 +11,42 @@ class MainHook : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         if (lpparam.packageName != "android" && lpparam.packageName != "com.android.settings") return
 
-        XposedBridge.hookAllMethods(
-            XposedHelpers.findClass(
-                "android.ext.settings.app.AswRestrictMemoryDynCodeLoading", lpparam.classLoader
-            ), "getImmutableValue", object : XC_MethodReplacement() {
-                override fun replaceHookedMethod(param: MethodHookParam?): Any? {
-                    val appInfo = param?.args[2] as? ApplicationInfo?
-
-                    // Settings has to always be allowed so that it can be patched as well.
-                    if (appInfo != null && appInfo.packageName == "com.android.settings") {
-                        return false
-                    }
-
-                    // All system apps are configurable (null)
-                    if (appInfo != null && (appInfo.flags and ApplicationInfo.FLAG_SYSTEM) != 0) {
-                        return null
-                    }
-
-                    // Defer to original implementation for other apps
-                    return XposedBridge.invokeOriginalMethod(
-                        param?.method, param?.thisObject, param?.args
-                    )
-                }
-            }
+        val DCL_MEM = "android.ext.settings.app.AswRestrictMemoryDynCodeLoading"
+        val DCL_STORAGE = "android.ext.settings.app.AswRestrictStorageDynCodeLoading"
+        val DCL_CLASSES = arrayOf(
+            DCL_MEM, DCL_STORAGE
         )
+
+        val replacement = object : XC_MethodReplacement() {
+            override fun replaceHookedMethod(param: MethodHookParam?): Any? {
+                val appInfo = param?.args?.getOrNull(2) as? ApplicationInfo
+
+                // Settings has to always be allowed DCL via memory so that it can be patched as well
+                if (appInfo != null && appInfo.packageName == "com.android.settings" && param.method?.declaringClass?.name == DCL_MEM) {
+                    return false
+                }
+
+                // All system apps are configurable (null)
+                if (appInfo != null && (appInfo.flags and ApplicationInfo.FLAG_SYSTEM) != 0) {
+                    return null
+                }
+
+                // Defer to original implementation for other apps
+                return XposedBridge.invokeOriginalMethod(
+                    param?.method, param?.thisObject, param?.args
+                )
+            }
+        }
+
+        for (className in DCL_CLASSES) {
+            val dclClass =
+                try {
+                    XposedHelpers.findClass(className, lpparam.classLoader)
+                } catch (_: XposedHelpers.ClassNotFoundError) {
+                    // Class not found, we are not on GrapheneOS
+                    return
+                }
+            XposedBridge.hookAllMethods(dclClass, "getImmutableValue", replacement)
+        }
     }
 }
